404d91cfa99d517bc7b12fa782b455f955ff921e
Suresh J <63852803+sureshtechi@users.noreply.github.com>
2021-06-08 22:53:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 
-// app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));  
 
 app.use(bodyParser.json());
 
